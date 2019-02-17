101d0b8b260c327b42e2457c45acec1ff3422f1e
ej <ejames788@gmail.com>
2019-02-17 17:56:35 -0500

    Added wall script

---
+++ Assets/Scripts/GameScripts/Wall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite WallDamageSprite;
+    public int HitPoints = 4;
+
+    //reference to this Wall component's Sprite Renderer component.
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = WallDamageSprite;
+        HitPoints -= loss;
+
+        //if player finally destroys the wall.
+        if (HitPoints <= 0)
+            gameObject.SetActive(false);
+    }
+
+}
